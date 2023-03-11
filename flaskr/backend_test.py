40b2866a88bfeeb23f51128faa77dc7a65b20c26
Kristofer Valerio <krisvalerio02@techexchange.in>
2023-03-11 20:41:25 +0000

    Testing init from backend

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -1,3 +1,13 @@
 from flaskr.backend import Backend
+from google.cloud import storage
+import pytest
 
 # TODO(Project 1): Write tests for Backend methods.
+@pytest.fixture
+def backend():
+    return Backend('wiki-backend-test-bucket')
+
+def test_init(backend):
+    assert backend.bucket_name == 'wiki-backend-test-bucket'
+    assert isinstance(backend.client, storage.Client)
+    assert isinstance(backend.bucket, storage.Bucket)
