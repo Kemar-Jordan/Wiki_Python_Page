647cb8033a2c84845e9bbf7f28ceec0f4aa248e9
Kemar Jordan <kemarjordan@techexchange.in>
2023-04-05 22:16:46 +0000

    Updated deploy changes

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -5,18 +5,15 @@
 # import pytest
 # import tempfile
 
-
 # @pytest.fixture
 # def backend():
 #     return Backend('wiki-backend-test-bucket')
-
 
 # # Testing __init__ method
 # def test_init(backend):
 #     assert backend.bucket_name == 'wiki-backend-test-bucket'
 #     assert isinstance(backend.client, storage.Client)
 #     assert isinstance(backend.bucket, storage.Bucket)
-
 
 # # Testing get_wiki_page method
 # def test_get_wiki_page(backend):
@@ -29,7 +26,6 @@
 #         url = backend.get_wiki_page('test-file')
 #         assert url == blob.public_url
 
-
 # # Test #1 for sign_up, testing the sign up of a new user with a valid username and password, should return True
 # def test_sign_up_1(backend):
 #     username = 'new_valid_user'
@@ -38,7 +34,6 @@
 #     # Delete user after testing the sign up
 #     assert backend.delete_user(username) == True
 
-
 # # Test #2 for sign_up method, testing the sign up of existing users, the method should return False
 # def test_sign_up_2(backend):
 #     username = 'existing_user'
@@ -46,7 +41,6 @@
 #     blob = backend.bucket.blob(username)
 #     blob.upload_from_string('existing_password_hash')
 #     assert backend.sign_up(username, password) == False
-
 
 # # Test #3 sign_up method, testing if a password is correctly hashed
 # def test_sign_up_3(backend):
@@ -61,7 +55,6 @@
 #         prefixed_password.encode()).hexdigest()
 #     assert bucket_password_hash == expected_hashed_password
 
-
 # # Test #1 get_image method, testing if we are returned the correct URL for an image
 # def test_get_image_1(backend):
 #     image_name = 'test_image.jpg'
@@ -72,7 +65,6 @@
 #         result = backend.get_image(image_name)
 #     assert result == 'https://storage.googleapis.com/wiki-backend-test-bucket/test_image.jpg'
 
-
 # # Test #2 get_image method, we pass through a jpg that doesn't exist and expect to be return None
 # def test_get_image_2(backend):
 #     image = 'mock_nonexistent.jpg'
@@ -81,7 +73,6 @@
 #     with patch.object(backend.bucket, 'blob', return_value=mock_blob):
 #         result = backend.get_image(image)
 #     assert result is None
-
 
 # # Test #1 the upload method, this tests a situation where the upload is a success
 # def test_upload_1(backend):
@@ -92,7 +83,6 @@
 #         backend.upload(temp_file.name, temp_file_name)
 #         blob = backend.bucket.blob(f"wiki-user-uploads/{temp_file_name}")
 #         assert blob.exists()
-
 
 # # Testing the upload method, this tests a situation where the upload is a failure
 # def test_upload_2(backend):
--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -4,7 +4,6 @@
 # import pytest
 # import io
 
-
 # # See https://flask.palletsprojects.com/en/2.2.x/testing/
 # # for more info on testing
 # @pytest.fixture
@@ -14,11 +13,9 @@
 #     })
 #     return app
 
-
 # @pytest.fixture
 # def client(app):
 #     return app.test_client()
-
 
 # # TODO(Checkpoint (groups of 4 only) Requirement 4): Change test to
 # # match the changes made in the other Checkpoint Requirements.
@@ -27,7 +24,6 @@
 #     resp = client.get("/")
 #     assert resp.status_code == 200
 #     assert b"The Python Wiki Server" in resp.data
-
 
 # #Test #1 for about page, testing author names
 # def test_about_page_1(client):
@@ -38,7 +34,6 @@
 #     assert b"Danielle McIntosh" in resp.data
 #     assert b"Kristofer Valerio" in resp.data
 
-
 # #Test #2 for about page, testing author images
 # def test_about_page_2(client):
 #     resp = client.get("/about")
@@ -47,13 +42,11 @@
 #     assert b'danielle.jpg' in resp.data
 #     assert b'kris.jpg' in resp.data
 
-
 # #Test #1 for pages page, testing title
 # def test_pages_page_1(client):
 #     resp = client.get("/pages")
 #     assert resp.status_code == 200
 #     assert b'Pages contained in this Wiki' in resp.data
-
 
 # # Test #2 for pages page, testing if the list of pages are displayed fully and correctly
 # def test_pages_page_2(client):
@@ -64,13 +57,11 @@
 #     for i in range(len(pages)):
 #         assert bytes(pages[i], 'utf-8') in resp.data
 
-
 # #Test #1 for log in (sign in) page, test that the page loads
 # def test_signin_page_1(client):
 #     resp = client.get("/signin")
 #     assert resp.status_code == 200
 #     assert b'Login' in resp.data
-
 
 # #Test #2 for log in (sign in) page, test the POST method where a user successfully logs in
 # def test_signin_page_2(client):
@@ -83,7 +74,6 @@
 #     assert b'testuser' in resp.data
 #     #assert b'logged_in.html' in resp.data
 
-
 # #Test #3 for log in (sign in) page, test the POST method in which a user inputs invalid login info
 # def test_signin_page_3(client):
 #     resp = client.post("/signin",
@@ -94,7 +84,6 @@
 #     assert resp.status_code == 200
 #     assert b'ERROR: Your login attempt has failed. Make sure the username and password are correct.' in resp.data
 
-
 # #Test #1 for sign up page, test when a user attempts to sign up with an existing username
 # def test_signup_page_1(client):
 #     resp = client.post("/signup",
@@ -104,7 +93,6 @@
 #                        })
 #     assert resp.status_code == 200
 #     assert b'Username already exists' in resp.data
-
 
 # # Test #2 for sign up page, test when a user successfully signs up with a valid username and password
 # def test_signup_page_2(client):
@@ -117,7 +105,6 @@
 #     # Delete the mock user after testing, test that the user is successfully deleted
 #     backend = Backend('wiki-credentials')
 #     assert backend.delete_user('mockuser') == True
-
 
 # # Test #1 for upload page, test a successful upload
 # def test_upload_page_1(client):
@@ -133,7 +120,6 @@
 #     assert resp.status_code == 200
 #     assert b'test-wiki has been uploaded successfully!' in resp.data
 
-
 # # Test #2 for upload page, test that whenever a wikiname is missing, the status code will be 400 - which indicates the server won't process the request due to client error
 # def test_upload_page_2(client):
 #     with client.session_transaction() as session:
@@ -143,7 +129,6 @@
 #         data={'wiki': (io.BytesIO(b'my file contents'), 'testfile.txt')},
 #         content_type='multipart/form-data')
 #     assert resp.status_code == 400
-
 
 # # Test #1 for log out page, test that the log out route will lead the user back to main.html
 # def test_logout_page_1(client):
