23a5587fd0d4cf92216ab57ec78d2259525a9aaf
alexbode <alexbode@techexchange.in>
2023-04-20 17:06:31 +0000

    fix unittests and use mock object for gcp bucket

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -1,18 +1,17 @@
-# def test_works():
-#     assert True
-
-
 from flaskr import create_app
 from unittest.mock import patch
 from flaskr.backend import Backend
 import pytest
 import io
 
+
+db_client_mock = None
+
 # See https://flask.palletsprojects.com/en/2.2.x/testing/
 # for more info on testing
 @pytest.fixture
 def app():
-    app = create_app({
+    app = create_app(test_config={
         'TESTING': True,
     })
     return app
@@ -27,63 +26,54 @@ def client(app):
 def test_home_page(client):
     resp = client.get("/")
     assert resp.status_code == 200
-    assert b"The Python Wiki Server" in resp.data
+    assert "The Python Wiki Server" in resp.text
 
 #Test #1 for about page, testing author names
 def test_about_page_1(client):
     resp = client.get("/about")
     assert resp.status_code == 200
-    assert b"Founding Authors" in resp.data
-    assert b"Kemar Jordan" in resp.data
-    assert b"Danielle McIntosh" in resp.data
-    assert b"Kristofer Valerio" in resp.data
+    assert "Founding Authors" in resp.text
+    assert "Kemar Jordan" in resp.text
+    assert "Danielle McIntosh" in resp.text
+    assert "Kristofer Valerio" in resp.text
 
-#Test #2 for about page, testing author images
-def test_about_page_2(client):
-    resp = client.get("/about")
-    assert resp.status_code == 200
-    assert b'kemar_j.jpg' in resp.data
-    assert b'danielle.jpg' in resp.data
-    assert b'kris.jpg' in resp.data
-
-#Test #1 for log in (sign in) page, test that the page loads
+# #Test #1 for log in (sign in) page, test that the page loads
 def test_signin_page_1(client):
     resp = client.get("/signin")
     assert resp.status_code == 200
-    assert b'Login' in resp.data
+    assert 'Login' in resp.text
 
-#Test #2 for log in (sign in) page, test the POST method where a user successfully logs in
+# #Test #2 for log in (sign in) page, test the POST method where a user successfully logs in
 def test_signin_page_2(client):
     resp = client.post("/signin",
                        data={
-                           'username': 'testuser',
-                           'password': 'testpassword'
+                           'username': 'test-user',
+                           'password': 'test-password'
                        })
     assert resp.status_code == 200
-    assert b'testuser' in resp.data
-    #assert b'logged_in.html' in resp.data
+    assert 'Welcome test-user!' in resp.text
 
-#Test #3 for log in (sign in) page, test the POST method in which a user inputs invalid login info
+# #Test #3 for log in (sign in) page, test the POST method in which a user inputs invalid login info
 def test_signin_page_3(client):
     resp = client.post("/signin",
                        data={
-                           'username': 'testuser',
+                           'username': 'test-user',
                            'password': 'wrongpassword'
                        })
     assert resp.status_code == 200
-    assert b'ERROR: Your login attempt has failed. Make sure the username and password are correct.' in resp.data
+    assert 'ERROR: Your login attempt has failed. Make sure the username and password are correct.' in resp.text
 
-#Test #1 for sign up page, test when a user attempts to sign up with an existing username
+# #Test #1 for sign up page, test when a user attempts to sign up with an existing username
 def test_signup_page_1(client):
     resp = client.post("/signup",
                        data={
-                           'username': 'testuser',
+                           'username': 'test-user',
                            'password': 'testpassword'
                        })
     assert resp.status_code == 200
-    assert b'Username already exists' in resp.data
+    assert 'Username already exists' in resp.text
 
-# Test #2 for sign up page, test when a user successfully signs up with a valid username and password
+# # Test #2 for sign up page, test when a user successfully signs up with a valid username and password
 def test_signup_page_2(client):
     resp = client.post("/signup",
                        data={
@@ -91,11 +81,9 @@ def test_signup_page_2(client):
                            'password': 'mockpassword'
                        })
     assert resp.status_code == 302
-    # Delete the mock user after testing, test that the user is successfully deleted
-    backend = Backend('wiki-credentials')
-    assert backend.delete_user('mockuser') == True
 
-# Test #1 for upload page, test a successful upload
+
+# # Test #1 for upload page, test a successful upload
 def test_upload_page_1(client):
     with client.session_transaction() as session:
         session['username'] = 'testuser'
@@ -107,9 +95,9 @@ def test_upload_page_1(client):
         },
         content_type='multipart/form-data')
     assert resp.status_code == 200
-    assert b'test-wiki has been uploaded successfully!' in resp.data
+    assert 'test-wiki has been uploaded successfully!' in resp.text
 
-# Test #2 for upload page, test that whenever a wikiname is missing, the status code will be 400 - which indicates the server won't process the request due to client error
+# # Test #2 for upload page, test that whenever a wikiname is missing, the status code will be 400 - which indicates the server won't process the request due to client error
 def test_upload_page_2(client):
     with client.session_transaction() as session:
         session['username'] = 'testuser'
@@ -119,8 +107,8 @@ def test_upload_page_2(client):
         content_type='multipart/form-data')
     assert resp.status_code == 400
 
-# Test #1 for log out page, test that the log out route will lead the user back to main.html
+# # Test #1 for log out page, test that the log out route will lead the user back to main.html
 def test_logout_page_1(client):
     resp = client.get("/logout")
     assert resp.status_code == 200
-    assert b"The Python Wiki Server" in resp.data
+    assert "The Python Wiki Server" in resp.text
